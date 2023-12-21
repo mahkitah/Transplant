b6eb35d8204bbc3cb5b6e2e7d99b49f8c816c0c1
mahkitah <mahkitah@protonmail.com>
2023-12-21 21:48:50 +0100

    upload rework

--- gazelle/upload.py
+++ gazelle/upload.py
@@ -1,72 +1,69 @@
 from bcoding import bencode, bdecode
-from gazelle.tracker_data import tr, RELEASE_TYPE_MAP
+from gazelle.tracker_data import tr, ReleaseType, ArtistType
 
-class FormData:
-    field_mapping = {
-        'edition': {
-            'unknown': 'unknown',
-            'remastered': 'remaster',
-            'rem_year': 'remaster_year',
-            'rem_title': 'remaster_title',
-            'rem_label': 'remaster_record_label',
-            'rem_cat_nr': 'remaster_catalogue_number',
-            'scene': 'scene',
-            'medium': 'media',
-            'format': 'format',
-            'encoding': 'bitrate',
-            'other_bitrate': 'other_bitrate',
-            'vbr_bitrate': 'vbr',
-            'rel_descr': 'release_desc',
-            'request_id': 'requestid',
-            'extra_format': 'extra_format[]',
-            'extra_encoding': 'extra_bitrate[]',
-            'extra_rel_descr': 'extra_release_desc[]'
-        },
-        'group': {
-            'rel_type': 'releasetype',
-            'artists': 'artists[]',
-            'importances': 'importance[]',
-            'title': 'title',
-            'o_year': 'year',
-            'tags': 'tags',
-            'upl_img_url': 'image',
-            'vanity': 'vanity_house',
-            'alb_descr': 'album_desc'
-        }
+
+FIELD_MAPPING = {
+    'edition': {
+        'unknown': 'unknown',
+        'remastered': 'remaster',
+        'rem_year': 'remaster_year',
+        'rem_title': 'remaster_title',
+        'rem_label': 'remaster_record_label',
+        'rem_cat_nr': 'remaster_catalogue_number',
+        'scene': 'scene',
+        'medium': 'media',
+        'format': 'format',
+        'encoding': 'bitrate',
+        'other_bitrate': 'other_bitrate',
+        'vbr': 'vbr',
+        'rel_descr': 'release_desc',
+        'request_id': 'requestid',
+        'extra_format': 'extra_format[]',
+        'extra_encoding': 'extra_bitrate[]',
+        'extra_rel_descr': 'extra_release_desc[]'
+    },
+    'group': {
+        'rel_type': 'releasetype',
+        'title': 'title',
+        'o_year': 'year',
+        'tags': 'tags',
+        'upl_img_url': 'image',
+        'vanity': 'vanity_house',
+        'alb_descr': 'album_desc'
     }
+}
 
+
+class UploadData:
     def __init__(self):
-        self.rel_type_name = None
-        self.artists = None
-        self.importances = None
-        self.title = None
-        self.o_year = None
-        self.unknown = False
-        self.remastered = False
-        self.rem_year = None
-        self.rem_title = None
-        self.rem_label = None
-        self.rem_cat_nr = None
-        self.scene = False
-        self.medium = None
-        self.format = None
-        self.encoding = None
-        self.other_bitrate = None
-        self.vbr_bitrate = False
-        self.vanity = False
-        self.tags = None
-        self.upl_img_url = None
-        self.alb_descr = None
-        self.rel_descr = None
-        self.request_id = None
-        self.extra_format = None
-        self.extra_encoding = None
-        self.extra_rel_descr = None
+        self.rel_type: ReleaseType | None = None
+        self.artists: dict[str, list[ArtistType]] | None = None
+        self.title: str | None = None
+        self.o_year: int | None = None
+        self.unknown: bool = False
+        self.remastered: bool = True
+        self.rem_year: int | None = None
+        self.rem_title: str | None = None
+        self.rem_label: str | None = None
+        self.rem_cat_nr: str | None = None
+        self.scene: bool = False
+        self.medium: str | None = None
+        self.format: str | None = None
+        self.encoding: str | None = None
+        self.other_bitrate: str | None = None
+        self.vbr: bool = False
+        self.vanity: bool = False
+        self.tags: str | None = None
+        self.upl_img_url: str | None = None
+        self.alb_descr: str | None = None
+        self.rel_descr: str | None = None
+        self.request_id: int | None = None
+        self.extra_format: str | None = None
+        self.extra_encoding: str | None = None
+        self.extra_rel_descr: str | None = None
+        self.src_tr: tr | None = None
 
-        self.src_tr = None
-        self.unknown = False
-
-    def _get_field(self, name, dest):
+    def _get_field(self, name, dest: tr):
 
         if name == 'rel_type':
             return self.rel_type.tracker_value(dest)
@@ -80,13 +77,22 @@ class FormData:
 
     def upl_dict(self, dest, dest_group=None):
 
-        field_map = self.field_mapping['edition'].copy()
+        field_map = FIELD_MAPPING['edition'].copy()
         upl_data = {'type': 0}
 
         if dest_group:
             upl_data['groupid'] = dest_group
         else:
-            field_map.update(self.field_mapping['group'])
+            field_map.update(FIELD_MAPPING['group'])
+
+            artists = []
+            importances = []
+            for artist_name, a_types in self.artists.items():
+                for a_type in a_types:
+                    artists.append(artist_name)
+                    importances.append(a_type.value)
+            upl_data['artists[]'] = artists
+            upl_data['importance[]'] = importances
 
         for k, v in field_map.items():
             value = self._get_field(k, dest)
@@ -98,13 +104,11 @@ class FormData:
                 upl_data['remaster_year'] = '1990'
                 upl_data['remaster_title'] = 'Unknown release year'
 
-        elif dest == tr.OPS:
-            if self.unknown:
-                # BUG There has to be a rem.year > 1982
-                # https://orpheus.network/forums.php?action=viewthread&threadid=10003
-                # https://orpheus.network/forums.php?action=viewthread&threadid=5994
-                upl_data['remaster_year'] = '1990'
+            if self.rel_type == ReleaseType.Sampler:
+                self.rel_type = ReleaseType.Compilation
 
+        elif dest == tr.OPS:
+            upl_data['workaround_broken_html_entities'] = 0
             if self.medium == 'Blu-Ray':
                 upl_data['media'] = 'BD'
 
@@ -179,7 +183,7 @@ class Files:
                 yield f'extra_file_{index}'
             index += 1
 
-    def files_list(self, announce=None, source=None):
+    def files_list(self, announce=None, source=None) -> list:
         files = []
         for field_name, dtor, i in zip(self.tor_field_names(), self.dtors, range(len(self.dtors))):
             if announce or source:
