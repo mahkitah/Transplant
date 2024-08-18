5769146b2c11122ade938a3fe7105bd866b47a4f
mahkitah <mahkitah@protonmail.com>
2024-08-19 00:22:07 +0200

    MV classes tweaks

--- GUI/mv_classes.py
+++ GUI/mv_classes.py
@@ -138,7 +138,7 @@ class JobModel(QAbstractTableModel):
                 return job.dest_group
 
         if role == Qt.ItemDataRole.CheckStateRole and column == 2:
-            return Qt.CheckState.Checked if job.new_dtor else Qt.CheckState.Unchecked
+            return Qt.CheckState(job.new_dtor * 2)
 
         if role == Qt.ItemDataRole.DecorationRole and column == 0 and not no_icon:
             return self.icons[job.src_tr]
@@ -184,7 +184,8 @@ class JobModel(QAbstractTableModel):
             job.dest_group = value or None
 
         if column == 2 and role == Qt.ItemDataRole.CheckStateRole:
-            job.new_dtor = Qt.CheckState(value) == Qt.CheckState.Checked
+            value: int
+            job.new_dtor = Qt.CheckState(value) is Qt.CheckState.Checked
 
         return True
 
@@ -225,7 +226,7 @@ class JobModel(QAbstractTableModel):
         yield numbers[-1], numbers[start_idx]
 
     def clear(self):
-        self.remove_jobs(0, self.rowCount(None) - 1)
+        self.remove_jobs(0, self.rowCount() - 1)
 
     def remove_jobs(self, first, last):
         self.beginRemoveRows(QModelIndex(), first, last)
@@ -284,9 +285,9 @@ class RehostModel(QAbstractTableModel):
             return super().flags(index)
 
     def headerData(self, section: int, orientation: Qt.Orientation, role: int = 0):
-        if role == Qt.ItemDataRole.DisplayRole and orientation == Qt.Orientation.Horizontal:
+        if role == Qt.ItemDataRole.DisplayRole and orientation is Qt.Orientation.Horizontal:
             return self.column_names[section]
-        if role == Qt.ItemDataRole.DisplayRole and orientation == Qt.Orientation.Vertical:
+        if role == Qt.ItemDataRole.DisplayRole and orientation is Qt.Orientation.Vertical:
             return IH(section).prio + 1
         else:
             return super().headerData(section, orientation, role)
@@ -300,7 +301,9 @@ class RehostModel(QAbstractTableModel):
                 return False
             host.key = value
         if column == 0 and role == Qt.ItemDataRole.CheckStateRole:
-            host.enabled = True if value == Qt.CheckState.Checked.value else False
+            value: int
+            host.enabled = Qt.CheckState(value) is Qt.CheckState.Checked
+
         self.dataChanged.emit(index, index, [role])
         return True
 
